604377cd0bbfc47c5eeb961197465e7f6e0091f9
raihan <ryihan55@gmail.com>
2021-04-29 05:10:35 +0600

    Create aws_publish.php

---
+++ config/aws_publish.php
@@ -0,0 +1,13 @@
+<?php
+use Aws\Laravel\AwsServiceProvider;
+return [
+    'credentials' => [
+        'key'    => env('AWS_ACCESS_KEY_ID', ''),
+        'secret' => env('AWS_SECRET_ACCESS_KEY', ''),
+    ],
+    'region' => env('AWS_REGION', 'us-east-1'),
+    'version' => 'latest',
+    'ua_append' => [
+        'L5MOD/' . AwsServiceProvider::VERSION,
+    ],
+];
