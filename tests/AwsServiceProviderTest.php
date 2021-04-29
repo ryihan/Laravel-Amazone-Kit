fca59ba75a6fb1c95a8fbb1c35b8f13a806c9f20
raihan <ryihan55@gmail.com>
2021-04-29 13:55:21 +0600

    Update AwsServiceProviderTest.php

--- tests/AwsServiceProviderTest.php
+++ tests/AwsServiceProviderTest.php
@@ -4,4 +4,11 @@ use Aws\Laravel\AwsServiceProvider;
 use Illuminate\Container\Container;
 abstract class AwsServiceProviderTest extends \PHPUnit_Framework_TestCase
 {
+    private function setupServiceProvider(Container $app)
+    {
+        $provider = new AwsServiceProvider($app);
+        $app->register($provider);
+        $provider->boot();
+        return $provider;
+    }
 }
