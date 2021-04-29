10e079ea268395559142fe44af0126497c9f3191
raihan <ryihan55@gmail.com>
2021-04-29 13:57:17 +0600

    Update LaravelAwsServiceProviderTest.php

--- tests/LaravelAwsServiceProviderTest.php
+++ tests/LaravelAwsServiceProviderTest.php
@@ -3,4 +3,11 @@ use Illuminate\Config\Repository;
 use Illuminate\Foundation\Application;
 class LaravelAwsServiceProviderTest extends AwsServiceProviderTest
 {
+    protected function setupApplication()
+    {
+        $app = new Application();
+        $app->setBasePath(sys_get_temp_dir());
+        $app->instance('config', new Repository());
+        return $app;
+    }
 }
