0f1564b48ac495c4e25f6fb1ac628bf9ca9d4fc8
raihan <ryihan55@gmail.com>
2021-04-29 13:54:57 +0600

    Create AwsServiceProviderTest.php

---
+++ tests/AwsServiceProviderTest.php
@@ -0,0 +1,7 @@
+<?php namespace Aws\Laravel\Test;
+use Aws\Laravel\AwsFacade as AWS;
+use Aws\Laravel\AwsServiceProvider;
+use Illuminate\Container\Container;
+abstract class AwsServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+}
