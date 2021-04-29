0e51e0e27bad4b3720afb0a43f6274f7995c7e12
raihan <ryihan55@gmail.com>
2021-04-29 13:58:20 +0600

    Update LumenAwsServiceProviderTest.php

--- tests/LumenAwsServiceProviderTest.php
+++ tests/LumenAwsServiceProviderTest.php
@@ -4,5 +4,12 @@ use Illuminate\Config\Repository;
 use Laravel\Lumen\Application;
 class LumenAwsServiceProviderTest extends AwsServiceProviderTest
 {
+    public function setUp()
+    {
+        if (!class_exists(Application::class)) {
+            $this->markTestSkipped();
+        }
+        parent::setUp();
+    }
     }
 }
