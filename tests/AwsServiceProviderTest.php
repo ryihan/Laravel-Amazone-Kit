aee45877c4fe705e11a9e43b97716d45031361fc
raihan <ryihan55@gmail.com>
2021-04-29 13:55:52 +0600

    Update AwsServiceProviderTest.php

--- tests/AwsServiceProviderTest.php
+++ tests/AwsServiceProviderTest.php
@@ -4,6 +4,18 @@ use Aws\Laravel\AwsServiceProvider;
 use Illuminate\Container\Container;
 abstract class AwsServiceProviderTest extends \PHPUnit_Framework_TestCase
 {
+    public function testVersionInformationIsProvidedToSdkUserAgent()
+    {
+        $app = $this->setupApplication();
+        $this->setupServiceProvider($app);
+        $config = $app['config']->get('aws');
+        $this->assertArrayHasKey('ua_append', $config);
+        $this->assertInternalType('array', $config['ua_append']);
+        $this->assertNotEmpty($config['ua_append']);
+        $this->assertNotEmpty(array_filter($config['ua_append'], function ($ua) {
+            return false !== strpos($ua, AwsServiceProvider::VERSION);
+        }));
+    }
     abstract protected function setupApplication();
     private function setupServiceProvider(Container $app)
     {
