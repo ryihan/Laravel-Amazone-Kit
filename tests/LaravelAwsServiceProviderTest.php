e91f67e9cce784fd94f3e1304523520a321b3b4c
raihan <ryihan55@gmail.com>
2021-04-29 13:57:28 +0600

    Update LaravelAwsServiceProviderTest.php

--- tests/LaravelAwsServiceProviderTest.php
+++ tests/LaravelAwsServiceProviderTest.php
@@ -3,6 +3,13 @@ use Illuminate\Config\Repository;
 use Illuminate\Foundation\Application;
 class LaravelAwsServiceProviderTest extends AwsServiceProviderTest
 {
+    public function setUp()
+    {
+        if (!class_exists(Application::class)) {
+            $this->markTestSkipped();
+        }
+        parent::setUp();
+    }
     protected function setupApplication()
     {
         $app = new Application();
